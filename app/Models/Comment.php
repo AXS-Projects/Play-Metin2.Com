103916d924fa31525a8e968219975974fa234b39
AXS-Projects <alex@axs-projects.com>
2025-06-21 14:10:17 +0100

    Add news reactions and threaded comments

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,10 +9,20 @@ class Comment extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['news_id', 'author', 'content', 'likes', 'dislikes'];
+    protected $fillable = ['news_id', 'author', 'content', 'likes', 'dislikes', 'parent_id'];
 
     public function news()
     {
         return $this->belongsTo(News::class);
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Comment::class, 'parent_id');
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
     }
 }
