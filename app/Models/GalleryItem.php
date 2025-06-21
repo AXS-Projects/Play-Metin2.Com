b0af0cbf8ff3b20768134a30c75ae0fd36e678a8
AXS-Projects <alex@axs-projects.com>
2025-06-21 16:26:33 +0100

    Enhance gallery with comments restrictions and reactions

--- app/Models/GalleryItem.php
+++ app/Models/GalleryItem.php
@@ -4,15 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Reaction;
 
 class GalleryItem extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'image_path', 'video_url'];
+    protected $fillable = [
+        'title',
+        'image_path',
+        'video_url',
+        'author',
+        'views',
+        'likes',
+        'dislikes',
+    ];
 
     public function comments()
     {
         return $this->hasMany(GalleryComment::class);
+    }
+
+    public function reactions()
+    {
+        return $this->morphMany(Reaction::class, 'reactionable');
     }
 }
