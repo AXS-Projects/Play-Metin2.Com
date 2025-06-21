1e2ed1af6aa50645c6aa41d378176c11770edc3d
AXS-Projects <alex@axs-projects.com>
2025-06-21 12:02:01 +0100

    Add news categories and assign to news

---
+++ app/Filament/Resources/NewsCategoryResource/Pages/EditNewsCategory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\NewsCategoryResource\Pages;
+
+use App\Filament\Resources\NewsCategoryResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditNewsCategory extends EditRecord
+{
+    protected static string $resource = NewsCategoryResource::class;
+}
--- app/Filament/Resources/NewsResource.php
+++ app/Filament/Resources/NewsResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\NewsResource\Pages;
 use App\Models\News;
+use App\Models\NewsCategory;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -11,6 +12,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
 use Filament\Tables\Columns\TextColumn;
 
 class NewsResource extends Resource
@@ -26,6 +28,10 @@ class NewsResource extends Resource
                 TextInput::make('title')
                     ->required()
                     ->maxLength(255),
+                Forms\Components\Select::make('news_category_id')
+                    ->label('Category')
+                    ->relationship('category', 'name')
+                    ->required(),
                 RichEditor::make('content')
                     ->required(),
             ]);
@@ -36,6 +42,7 @@ class NewsResource extends Resource
         return $table
             ->columns([
                 TextColumn::make('title')->sortable()->searchable(),
+                TextColumn::make('category.name')->label('Category')->sortable(),
                 TextColumn::make('created_at')->dateTime()->sortable()->label('Created'),
             ])
             ->actions([
