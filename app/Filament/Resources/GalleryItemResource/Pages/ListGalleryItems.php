1ba0526d6ad01aab6eefb06aa7808dd9658e5ecb
AXS-Projects <alex@axs-projects.com>
2025-06-21 13:00:32 +0100

    Fix missing create button in GalleryItemResource

--- app/Filament/Resources/GalleryItemResource/Pages/ListGalleryItems.php
+++ app/Filament/Resources/GalleryItemResource/Pages/ListGalleryItems.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\GalleryItemResource\Pages;
 
 use App\Filament\Resources\GalleryItemResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListGalleryItems extends ListRecords
 {
     protected static string $resource = GalleryItemResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
